1f408cd245c5e1a831505a34f9c4a1a0d790ed2b
Reese Norton <reesenortonswift@gmail.com>
2025-02-09 08:27:35 -0500

    Initial Commit

---
+++ MealMate/MealMateApp.swift
@@ -0,0 +1,17 @@
+//
+//  MealMateApp.swift
+//  MealMate
+//
+//  Created by Reese Norton on 2/9/25.
+//
+
+import SwiftUI
+
+@main
+struct MealMateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MealMateTests/MealMateTests.swift
@@ -0,0 +1,17 @@
+//
+//  MealMateTests.swift
+//  MealMateTests
+//
+//  Created by Reese Norton on 2/9/25.
+//
+
+import Testing
+@testable import MealMate
+
+struct MealMateTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
